c4a45cd9b61d64901423418103876b75034ce045
Tobias Walle <tobias.walle@maibornwolff.de>
2019-03-12 20:22:25 +0100

    setup basic jooby application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    dependencies {
+        classpath("org.jooby:jooby-gradle-plugin:1.6.0")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.3.21"
+    application
 }
+
+apply(plugin = "jooby")
 
 group = "com.tobiaswalle"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClassName = "com.tobiaswalle.joobytestproject.MainKt"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    compile("org.jooby:jooby-apitool:1.6.0")
+    compile("org.jooby:jooby-lang-kotlin:1.6.0")
+    compile("org.jooby:jooby-undertow:1.6.0")
     implementation(kotlin("stdlib-jdk8"))
 }
 
--- src/main/kotlin/com/tobiaswalle/joobytestproject/main.kt
+++ src/main/kotlin/com/tobiaswalle/joobytestproject/main.kt
@@ -1,5 +1,25 @@
 package com.tobiaswalle.joobytestproject
 
-fun main() {
-    println("Hello World")
+import org.jooby.Kooby
+import org.jooby.apitool.ApiTool
+import org.jooby.run
+
+class SubRoute : Kooby({
+    get("hello") {
+        "word"
+    }
+})
+
+class App : Kooby({
+    use(ApiTool().swagger("/swagger"))
+
+    get("route") {
+        123
+    }
+
+    use("subroute", SubRoute())
+})
+
+fun main(args: Array<String>) {
+    run(::App, *args)
 }
